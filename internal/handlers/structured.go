54c895e39102fb263e2c13a1040cc37a9f9289dc
turbolytics.io <danny@turbolytics.io>
2025-04-21 20:24:05 -0400

    Structured batch handler

--- internal/handlers/structured.go
+++ internal/handlers/structured.go
@@ -1,1 +1,126 @@
 package handlers
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/apache/arrow-go/v18/arrow"
+	"github.com/apache/arrow-go/v18/arrow/array"
+	"github.com/apache/arrow-go/v18/arrow/memory"
+	"github.com/marcboeker/go-duckdb"
+	"sync"
+)
+
+type StructuredBatchHandler struct {
+	mu    sync.Mutex
+	batch []arrow.Record
+
+	alloc  *memory.GoAllocator
+	arr    *duckdb.Arrow
+	schema *arrow.Schema
+	sql    string
+
+	tableName string
+}
+
+func (h *StructuredBatchHandler) Init(ctx context.Context) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.batch = nil
+	rdr, err := h.arr.QueryContext(
+		ctx,
+		fmt.Sprintf(
+			"TRUNCATE TABLE %s;",
+			h.tableName,
+		),
+	)
+	if err != nil {
+		return err
+	}
+	rdr.Release()
+	return nil
+}
+
+func (h *StructuredBatchHandler) Write(r []byte) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	record, _, err := array.RecordFromJSON(
+		h.alloc,
+		h.schema,
+		bytes.NewReader(r),
+		array.WithMultipleDocs(),
+	)
+	if err != nil {
+		return err
+	}
+
+	h.batch = append(h.batch, record)
+	return nil
+}
+
+func (h *StructuredBatchHandler) Invoke(ctx context.Context) (arrow.Table, error) {
+	recordReader, err := array.NewRecordReader(
+		h.schema,
+		h.batch,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	release, err := h.arr.RegisterView(
+		recordReader,
+		"batch",
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer release()
+
+	copyRes, err := h.arr.QueryContext(
+		ctx,
+		fmt.Sprintf(
+			"INSERT INTO %s (SELECT * FROM batch)",
+			h.tableName,
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	copyRes.Release()
+
+	rdr, err := h.arr.QueryContext(
+		ctx,
+		h.sql,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rdr.Release()
+
+	var records []arrow.Record
+	for rdr.Next() {
+		records = append(records, rdr.Record())
+	}
+
+	table := array.NewTableFromRecords(
+		rdr.Schema(),
+		records,
+	)
+
+	return table, nil
+}
+
+func NewStructuredBatchHandler(arr *duckdb.Arrow, sql string, tableName string, schema *arrow.Schema) (*StructuredBatchHandler, error) {
+	pool := memory.NewGoAllocator()
+
+	s := &StructuredBatchHandler{
+		alloc:     pool,
+		arr:       arr,
+		schema:    schema,
+		sql:       sql,
+		tableName: tableName,
+	}
+	return s, nil
+}
--- internal/handlers/structured_test.go
+++ internal/handlers/structured_test.go
@@ -3,7 +3,7 @@ package handlers
 import (
 	"context"
 	"database/sql"
-	"fmt"
+	"github.com/apache/arrow-go/v18/arrow"
 	"github.com/marcboeker/go-duckdb"
 	_ "github.com/marcboeker/go-duckdb"
 	"github.com/zeebo/assert"
@@ -11,11 +11,18 @@ import (
 )
 
 func TestArrowGetSchema(t *testing.T) {
-	db, err := sql.Open("duckdb", ":memory:")
-	if err != nil {
-		t.Fatalf("failed to open duckdb: %v", err)
-	}
-	defer db.Close()
+	connector, err := duckdb.NewConnector(":memory:", nil)
+	assert.NoError(t, err)
+	defer func() {
+		err := connector.Close()
+		assert.NoError(t, err)
+	}()
+
+	db := sql.OpenDB(connector)
+	defer func() {
+		err := db.Close()
+		assert.NoError(t, err)
+	}()
 
 	// Create a table with 5 columns
 	createStmt := `
@@ -29,39 +36,92 @@ func TestArrowGetSchema(t *testing.T) {
 	`
 
 	_, err = db.Exec(createStmt)
-	if err != nil {
-		t.Fatalf("failed to create table: %v", err)
-	}
-
-	// Check that the table was created
-	rows, err := db.Query(`PRAGMA table_info('users');`)
-	if err != nil {
-		t.Fatalf("failed to query table info: %v", err)
-	}
-	defer rows.Close()
-
-	count := 0
-	for rows.Next() {
-		count++
-	}
-	assert.NoError(t, rows.Err())
-
-	if count != 5 {
-		t.Fatalf("expected 5 columns, got %d", count)
-	}
-
-	conn, err := db.Conn(context.Background())
-	assert.NoError(t, err)
-	defer conn.Close()
-
-	arrow, err := duckdb.NewArrowFromConn(conn)
 	assert.NoError(t, err)
 
-	rdr, err := arrow.QueryContext(
+	conn, err := connector.Connect(context.Background())
+	assert.NoError(t, err)
+	defer func() {
+		err := conn.Close()
+		assert.NoError(t, err)
+	}()
+
+	arr, err := duckdb.NewArrowFromConn(conn)
+	assert.NoError(t, err)
+
+	rdr, err := arr.QueryContext(
 		context.Background(),
 		"select * from users",
 	)
 	assert.NoError(t, err)
 
-	fmt.Println(rdr.Schema())
+	schema := rdr.Schema()
+	assert.Equal(t, 5, len(schema.Fields()))
+}
+
+func TestStructuredBatchHandler_SelectCount(t *testing.T) {
+	connector, err := duckdb.NewConnector(":memory:", nil)
+	assert.NoError(t, err)
+	defer func() {
+		err := connector.Close()
+		assert.NoError(t, err)
+	}()
+
+	db := sql.OpenDB(connector)
+	defer func() {
+		err := db.Close()
+		assert.NoError(t, err)
+	}()
+
+	// Create a table with 5 columns
+	createStmt := `
+	CREATE TABLE users (
+		id INTEGER,
+		name TEXT,
+		email TEXT,
+		age INTEGER,
+		created_at TIMESTAMP
+	);
+	`
+
+	_, err = db.Exec(createStmt)
+	assert.NoError(t, err)
+
+	conn, err := connector.Connect(context.Background())
+	assert.NoError(t, err)
+	defer func() {
+		err := conn.Close()
+		assert.NoError(t, err)
+	}()
+
+	arr, err := duckdb.NewArrowFromConn(conn)
+	assert.NoError(t, err)
+
+	schema := arrow.NewSchema(
+		[]arrow.Field{
+			{Name: "id", Type: arrow.PrimitiveTypes.Int32},
+			{Name: "name", Type: arrow.BinaryTypes.String},
+			{Name: "email", Type: arrow.BinaryTypes.String},
+			{Name: "age", Type: arrow.PrimitiveTypes.Int32},
+			{Name: "created_at", Type: arrow.FixedWidthTypes.Timestamp_ms},
+		},
+		nil)
+
+	h, err := NewStructuredBatchHandler(
+		arr,
+		"SELECT * FROM users",
+		"users",
+		schema,
+	)
+	assert.NoError(t, err)
+
+	err = h.Init(context.Background())
+	assert.NoError(t, err)
+
+	err = h.Write([]byte(`{"id": 1, "name": "test name", "email": "test email", "age": 77, "created_at": "2025-04-10 15:00:00"}`))
+	assert.NoError(t, err)
+
+	res, err := h.Invoke(context.Background())
+	assert.NoError(t, err)
+
+	assert.Equal(t, 1, res.NumRows())
 }
